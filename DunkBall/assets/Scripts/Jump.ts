8f9cae6f07d3463c26aa8a323cc2351f5c429cb6
DESKTOP-EM5MLDA\savyashree <savyashree.a@juegostudioz.com>
2021-03-15 12:11:13 +0530

    addeda function to  toggle basket

--- DunkBall/assets/Scripts/Jump.ts
+++ DunkBall/assets/Scripts/Jump.ts
@@ -17,13 +17,15 @@ export class Jump extends Component {
     @property
     isCollided = false;
 
+    @property
+    isRightBasketEnabled = true;
 
     private _rigidBody: RigidBodyComponent | undefined;
     
     
 
     onLoad () {
-        
+        console.log("Called on Load"+ this.isRightBasketEnabled);
           systemEvent.on(SystemEvent.EventType.MOUSE_DOWN, this.onMouseDown, this);
           systemEvent.on(SystemEvent.EventType.TOUCH_END, this.onTouchEnd, this);
 
@@ -36,56 +38,96 @@ export class Jump extends Component {
     }
 
     private onCollision (event: ICollisionEvent) {
-        //console.log( event.otherCollider,  event.selfCollider);
-        if(event.otherCollider.node.name=="ColliderForHoop"){
+        if(event.otherCollider.node.name=="ColliderForHoopRight" || event.otherCollider.node.name=="ColliderForHoopLeft"){
                 console.log("COLLIDED");
+                this._rigidBody?.setAngularVelocity(new math.Vec3(0,0,0));
                 event.otherCollider.node.active= false;
                 this.isCollided=true; 
                 
         }
         if(this.isCollided){
-            var hoop= find("ColliderForHoop");
-            console.log(hoop);
-            if(event.otherCollider.node.name=="Platform"){
-                console.log("COLLIDED with surface");
-                hoop.active= true;
-                this.isCollided=false; 
+            if(this.isRightBasketEnabled){
+                var colliderForHoopRight= find("basketball_hoop_Right/ColliderForHoopRight");
+                if(event.otherCollider.node.name=="Platform"){
+                    console.log("COLLIDED with surface");
+                    colliderForHoopRight.active= true;
+                    this.isCollided=false; 
+                    this.toggleBasket();
+                }
             }
+            else{
+                var colliderForHoopLeft= find("basketball_hoop_Left/ColliderForHoopLeft");
+                if(event.otherCollider.node.name=="Platform"){
+                    console.log("COLLIDED with surface");
+                    colliderForHoopLeft.active= true;
+                    this.isCollided=false; 
+                    this.toggleBasket();
+                }
+            }
+
         }
+    }
+
+    toggleBasket(){
+        var rightBasket= find("basketball_hoop_Right");
+        var leftBasket= find("basketball_hoop_Left");
+        if(this.isRightBasketEnabled)
+            this.enabledLeftBasket(rightBasket,leftBasket);
+        else
+            this.enabledRightBasket(rightBasket,leftBasket);
+    }
+
+    enabledRightBasket(rightBasket,leftBasket){
+        leftBasket.active= false;
+        rightBasket.active= true;
+        this.isRightBasketEnabled= true;
+    }
+    enabledLeftBasket(rightBasket,leftBasket){
+        
+        console.log("Called enabledLeftBasket");
+        leftBasket.active= true;
+        rightBasket.active= false;
+        this.isRightBasketEnabled= false;
     }
     
     onMouseDown(){
-        console.log("MOUSE down");
         //  tween(this.node)
         //     .to(this.jumpDuration, { position: new Vec3(this.node.getPosition().x, this.node.getPosition().y+this.jumpHeight, this.node.getPosition().z) }, { easing: 'cubicOut' })
         //     .to(this.jumpDuration, { position: new Vec3(this.node.getPosition().x, this.node.getPosition().y-this.jumpHeight, this.node.getPosition().z) }, { easing: 'cubicOut' })
         //     .union()
         //     .repeatForever()
         //     .start();
-        this._rigidBody?.applyImpulse(new math.Vec3(3,55,0));
-        // this.node.setRotation(new math.Quat(0,-100,-100,0));
+        console.log(this.isRightBasketEnabled)
+        if(this.isRightBasketEnabled)
+            this._rigidBody?.applyImpulse(new math.Vec3(3,60,0));
+        else
+            this._rigidBody?.applyImpulse(new math.Vec3(-3,60,0));
     }
+
+
     onTouchEnd(){
-        console.log("Touch end");
         //  tween(this.node)
         //     .to(this.jumpDuration, { position: new Vec3(this.node.getPosition().x, this.node.getPosition().y+this.jumpHeight, this.node.getPosition().z) }, { easing: 'cubicOut' })
         //     .to(this.jumpDuration, { position: new Vec3(this.node.getPosition().x, this.node.getPosition().y-this.jumpHeight, this.node.getPosition().z) }, { easing: 'cubicOut' })
         //     .union()
         //     .repeatForever()
         //     .start();
-        this._rigidBody?.applyImpulse(new math.Vec3(3,55,0));
+       // this._rigidBody?.applyImpulse(new math.Vec3(3,55,0));
         // this.node.setRotation(new math.Quat(0,-100,-100,0));
     }
 
 
     update (deltaTime: number) {
-        // [4]
-        //console.log(this.node.getWorldPosition() );
-         if(this.node.getPosition().x>18){
+        if(this.node.getPosition().x>18){
              this._rigidBody?.setAngularVelocity(new math.Vec3(0,0,0));
         //     this._rigidBody?.setLinearVelocity(new math.Vec3(0,0,0));
             this.node.setPosition(new math.Vec3(-18,this.node.getPosition().y,5));
         }
+        else if(this.node.getPosition().x<-18){
+            this._rigidBody?.setAngularVelocity(new math.Vec3(0,0,0));
+       //     this._rigidBody?.setLinearVelocity(new math.Vec3(0,0,0));
+           this.node.setPosition(new math.Vec3(18,this.node.getPosition().y,5));
+       }
         // console.log(this.node.getRotation().x,this.node.getRotation().y-100,this.node.getRotation().z-100,this.node.getRotation().w);
         // this.node.setRotation(new Quat(0,0,3.14/180*deltaTime));
     }
